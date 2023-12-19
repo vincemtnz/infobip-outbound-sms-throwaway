0ca03bc84740e12212d0e960742c1056390554ba
Vince Martinez <vince.martinez@klaviyo.com>
2023-12-19 11:36:19 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Outbound SMS test
+
+Testing Infobip outbound SMS with `notifyUrl`
+
+## Requirements
+
+1. [Bun](https://bun.sh)
+2. Infobip credentials (API url and key)
+3. A webhook endpoint. I used https://webhook.site
+
+## Setup
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
---
+++ index.ts
@@ -0,0 +1,41 @@
+import { Infobip, AuthType } from "@infobip-api/sdk";
+
+const BASE_URL = process.env.BASE_URL;
+const API_KEY = process.env.API_KEY;
+const SENDER_ID = process.env.SENDER_ID;
+const NOTIFY_URL = process.env.NOTIFY_URL;
+
+if (!API_KEY) {
+  throw new Error("API_KEY is required");
+}
+if (!BASE_URL) {
+  throw new Error("BASE_URL is required");
+}
+
+const recipients = await Bun.file("./recipients.json").json<
+  Array<{ name: string; number: string }>
+>();
+
+const payload = {
+  messages: recipients.map((recipient) => ({
+    destinations: [{ to: recipient.number }],
+    from: SENDER_ID,
+    notifyUrl: NOTIFY_URL,
+    notifyContentType: "application/json",
+    validityPeriod: 5, // in minutes
+    text: `Hi ${recipient.name}. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    callbackData: JSON.stringify({ foo: "bar" }),
+  })),
+};
+
+console.log(JSON.stringify(payload, null, 2));
+
+let client = new Infobip({
+  authType: AuthType.ApiKey,
+  baseUrl: BASE_URL ?? "",
+  apiKey: API_KEY,
+});
+
+const response = await client.channels.sms.send(payload).catch(console.error);
+
+console.log(JSON.stringify(response.data, null, 2));
